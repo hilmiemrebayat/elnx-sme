b9e798d1f23ad396025d96e770a471072ba5e927
Hilmi Emre Bayat <hilmi_emre@hotmail.com>
2017-12-14 16:13:23 +0100

    update

--- scripts/router-config.sh
+++ scripts/router-config.sh
@@ -10,31 +10,51 @@ delete system console device ttyS0
 # Basic settings
 #
 set system host-name 'Router'
-#set service ssh port '22'
+set system domain-name 'avalon.lan'
+set service ssh port '22'
 
 #
 # IP settings
 #
 
-# TODO
+set interfaces ethernet eth0 address dhcp
+set interfaces ethernet eth0 description "WAN link"
+
+set interfaces ethernet eth1 address 192.0.2.254/24
+set interfaces ethernet eth1 description DMZ
+
+set interfaces ethernet eth2 address 172.16.255.254/16
+set interfaces ethernet eth2 description internal
 
 #
 # Network Address Translation
 #
 
-# TODO
+set nat source rule 100 outbound-interface 'eth1'
+set nat source rule 100 translation address 'masquerade'
+set nat source rule 100 source address '172.16.0.0/16'
 
+set nat source rule 200 outbound-interface 'eth0'
+set nat source rule 200 translation address 'masquerade'
+set nat source rule 200 source address '172.16.0.0/16'
 #
 # Time
 #
 
-#
+delete system ntp server 0.pool.ntp.org
+delete system ntp server 1.pool.ntp.org
+delete system ntp server 2.pool.ntp.org
+set system ntp server 'be.pool.ntp.org'
+set system time-zone Europe/Brussels
 
 #
 # Domain Name Service
 #
 
-# TODO
+set service dns forwarding domain avalon.lan server 192.0.2.10
+set service dns forwarding name-server 10.0.2.3
+set service dns forwarding listen-on 'eth1'
+set service dns forwarding listen-on 'eth2'
 
 # Make configuration changes persistent
 commit
